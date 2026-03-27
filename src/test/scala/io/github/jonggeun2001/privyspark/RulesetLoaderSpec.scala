b3f9f1d6cc09b0cce8cfb22a52f5d4739b961b24
Jonggeun Kim <rla4509@naver.com>
2026-03-27 13:56:42 +0900

    fix: remove name detection support (#31)

--- src/test/scala/io/github/jonggeun2001/privyspark/RulesetLoaderSpec.scala
+++ src/test/scala/io/github/jonggeun2001/privyspark/RulesetLoaderSpec.scala
@@ -14,6 +14,7 @@ class RulesetLoaderSpec extends AnyFunSuite {
     val rules = RulesetLoader.load("default")
     assert(rules.nonEmpty)
     assert(rules.exists(_.piiType == "email"))
+    assert(!rules.exists(_.piiType == "name"))
     assert(rules.forall(_.columnHints.isEmpty))
     assert(rules.forall(_.matchType == "value"))
   }
@@ -71,8 +72,8 @@ class RulesetLoaderSpec extends AnyFunSuite {
     val rulesetPath = Files.createTempFile("privyspark-ruleset-validator", ".yaml")
     val yaml =
       """rules:
-        |  - pii_type: name
-        |    regex: '[가-힣]{2,4}'
+        |  - pii_type: email
+        |    regex: '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}'
         |    validator: korean_name_dict
         |""".stripMargin
 
@@ -87,11 +88,30 @@ class RulesetLoaderSpec extends AnyFunSuite {
     }
   }
 
+  test("throws when ruleset defines removed name pii type") {
+    val rulesetPath = Files.createTempFile("privyspark-ruleset-name", ".yaml")
+    val yaml =
+      """rules:
+        |  - pii_type: name
+        |    regex: '[가-힣]{2,4}'
+        |""".stripMargin
+
+    Files.write(rulesetPath, yaml.getBytes(StandardCharsets.UTF_8))
+    try {
+      val error = intercept[IllegalArgumentException] {
+        RulesetLoader.load(rulesetPath.toString)
+      }
+      assert(error.getMessage.contains("pii_type 'name' is no longer supported"))
+    } finally {
+      Files.deleteIfExists(rulesetPath)
+    }
+  }
+
   test("throws when ruleset still uses removed internal regex reference") {
     val rulesetPath = Files.createTempFile("privyspark-ruleset-regex-ref", ".yaml")
     val yaml =
       """rules:
-        |  - pii_type: name
+        |  - pii_type: email
         |    regex: '__KOREAN_NAME_RULE_REGEX__'
         |""".stripMargin
 
@@ -110,7 +130,7 @@ class RulesetLoaderSpec extends AnyFunSuite {
     val rulesetPath = Files.createTempFile("privyspark-ruleset-regex-ref-embedded", ".yaml")
     val yaml =
       """rules:
-        |  - pii_type: name
+        |  - pii_type: email
         |    regex: '(?:__KOREAN_NAME_RULE_REGEX__)|[가-힣]{2,4}'
         |""".stripMargin
 
