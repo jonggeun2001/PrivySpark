9e138914c37e7f53f64f7b9456b3b1626ec4383e
Jonggeun Kim <rla4509@naver.com>
2026-04-14 11:00:50 +0900

    fix: 기본 ruleset regex 오탐 억제 (#72)
    * fix: tighten default ruleset regexes
    
    * fix: tighten default email and mastercard prefixes
    
    * fix: relax ip punctuation boundary

--- src/test/scala/io/github/jonggeun2001/privyspark/PrivySparkAppSpec.scala
+++ src/test/scala/io/github/jonggeun2001/privyspark/PrivySparkAppSpec.scala
@@ -1190,14 +1190,15 @@ class PrivySparkAppSpec extends AnyFunSuite with BeforeAndAfterAll {
           "carol,XM12345678\n" +
           "dave,M12345678Y\n" +
           "erin,m12345678\n" +
-          "frank,M1234567\n")
+          "frank,M1234567\n" +
+          "grace,S00000000\n")
 
       val rules = Seq(RulesetLoader.load("default").find(_.piiType == "passport_number").get)
       val (results, errors) = scanWithRules(inputDir.toString, inputDir.toString, rules, timestamp)
 
       assert(errors.isEmpty)
       assert(results.map(result => (result.column_name, result.pii_type, result.match_count, result.match_ratio)).toSet ==
-        Set(("passport_no", "passport_number", 2L, 0.33)))
+        Set(("passport_no", "passport_number", 2L, 0.29)))
     } finally {
       deleteRecursively(inputDir)
     }
