104d97887ab385e0afd9497f8663319343b854c1
Jonggeun Kim <rla4509@naver.com>
2026-05-04 10:47:23 +0900

    refactor: remove GroupScanner wrapper (#186)

--- src/main/scala/io/github/jonggeun2001/privyspark/scan/GroupScanner.scala
+++
@@ -1,9 +0,0 @@
-package io.github.jonggeun2001.privyspark.scan
-
-@deprecated("use GroupScanCoordinator and FileSampling", "1.5.0")
-private[privyspark] object GroupScanner {
-  @deprecated("use FileSampling.selectSampledFileKeys", "1.5.0")
-  def selectSampledFileKeys(fileKeys: Seq[String], fileSampleRatio: Double, samplingContext: String = ""): Seq[String] = {
-    FileSampling.selectSampledFileKeys(fileKeys, fileSampleRatio, samplingContext)
-  }
-}
